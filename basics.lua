610fc4fdbeda2c90ce38227009c33f3fd1628a84
Iwan Burel <iwan.burel@gmail.com>
2021-05-29 12:03:44 +0200

    Add pulse

--- basics.lua
+++ basics.lua
@@ -13,3 +13,22 @@ function printMethods(side)
 		print(method)
 	end
 end
+
+function pulse(side, timer1, timer2)
+	expect(1, side, "string")
+	expect(2, timer1, "number", "nil")
+	expect(3, timer2, "number", "nil")
+	
+	if timer1 == nil then
+		timer1 = 0.1
+	end
+	
+	if timer2 == nil then
+		timer2 = 0.1
+	end
+	
+	redstone.setOutput(side, true)
+	sleep(timer1)
+	redstone.setOutput(side, false)
+	sleep(timer2)
+end
