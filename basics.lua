2281e0472e976459edb45f52d2bb7db668a9ca03
Iwan Burel <iwan.burel@gmail.com>
2021-05-29 11:39:41 +0200

    printMethods: Handle errors

--- basics.lua
+++ basics.lua
@@ -1,5 +1,14 @@
+local expect = dofile("/rom/modules/main/cc/expect.lua").expect
+
 function printMethods(side)
-	local methods = peripherical.getMethods(side)
+	expect(1, side, "string")
+
+	local methods = peripheral.getMethods(side)
+	if methods == nil then
+		print("No methods")
+		return
+	end
+	
 	for _, method in pairs(methods) do
 		print(method)
 	end
